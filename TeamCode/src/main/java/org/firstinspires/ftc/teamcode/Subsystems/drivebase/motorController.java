d282e51260236693e22ef897f45985552f589553
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2025-05-06 15:50:10 +1000

    Update motorController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/drivebase/motorController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/drivebase/motorController.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.Subsystems.drivebase;
-
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 public class motorController {
-    new
+    DcMotor FrontLeft;
     static void setMotorSpeeds(double FL, double BL, double FR, double BR){
 
     }
