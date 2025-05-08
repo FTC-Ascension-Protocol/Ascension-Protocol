8baf2c2532a25bd2b340d2a44d6b99c74ab2bc43
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2025-05-09 09:21:21 +1000

    Create PIDController.java

---
+++ FtcRobotController/PIDController.java
@@ -0,0 +1,9 @@
+public class PIDController {
+    //PID loop
+    /*
+    while True:
+     targetVelocity = getTargetVelocity(time)
+     targetAcceleration = getTargetAcceleration(time)
+     output = targetVelocity * Kv + targetAcceleration * Ka;
+     */
+}
