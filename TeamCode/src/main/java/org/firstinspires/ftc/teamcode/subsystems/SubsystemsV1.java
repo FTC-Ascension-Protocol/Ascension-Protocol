29502980630b87aa2a9c3fa148cecaf97c945aa7
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2025-05-08 16:25:16 +1000

    Working Drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SubsystemsV1.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.subsystems.drivebase.MotorController;
+
+@TeleOp(name="SubsystemsV1", group="Linear OpMode")
+public class SubsystemsV1 extends LinearOpMode {
+    MotorController drivebase = new MotorController();
+    Gamepad gamepad1;
+    @Override
+    public void runOpMode(){
+        drivebase.initialiseDrivebaseMotors(
+                hardwareMap.get(DcMotor.class, "frontLeftMotor"),
+                hardwareMap.get(DcMotor.class, "backLeftMotor"),
+                hardwareMap.get(DcMotor.class, "frontRightMotor"),
+                hardwareMap.get(DcMotor.class, "backRightMotor")
+        );
+        gamepad1 = hardwareMap.get(Gamepad.class, "GamepadA");
+        waitForStart();
+
+        while(opModeIsActive()){
+            drivebase.drive(gamepad1);
+        }
+    }
+
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivebase/MotorController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivebase/MotorController.java
@@ -1,34 +1,40 @@
-package org.firstinspires.ftc.teamcode.Subsystems.drivebase;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+package org.firstinspires.ftc.teamcode.subsystems.drivebase;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
-
-import java.util.Vector;
-
-public class motorController {
+public class MotorController {
     DcMotor frontLeftMotor;
     DcMotor frontRightMotor;
     DcMotor backLeftMotor;
     DcMotor backRightMotor;
-    Gamepad gamepad1;
-    void setMotorSpeeds(Vector vector){
-        double forwardSpeed = -gamepad1.left_stick_y;
-        double turnSpeed = gamepad1.left_stick_x;
-        double strafeSpeed = gamepad1.right_stick_x;
+    public void initialiseDrivebaseMotors(DcMotor FL, DcMotor BL, DcMotor FR, DcMotor BR) {
+        //Initialise the motors
+        frontLeftMotor = FL;
+        backLeftMotor = BL;
+        frontRightMotor = FR;
+        backRightMotor = BR;
+    }
 
+    void setMotorSpeeds(double forwardSpeed, double turnSpeed, double strafeSpeed){
+
+        //control the motors with X, Y and rotate.
         double frontLeftSpeed = forwardSpeed + turnSpeed + strafeSpeed;
         double backLeftSpeed = forwardSpeed + turnSpeed - strafeSpeed;
-        double frontRightSpeed = forwardSpeed - turnSpeed -strafeSpeed;
+        double frontRightSpeed = forwardSpeed - turnSpeed - strafeSpeed;
         double backRightSpeed = forwardSpeed - turnSpeed + strafeSpeed;
 
+        //Find max to prevent value saturation
         double max1 = Math.max(Math.abs(frontLeftSpeed), Math.abs(backLeftSpeed));
         double max2 = Math.max(Math.abs(frontRightSpeed), Math.abs(backRightSpeed));
         double max3 = Math.max(max1, max2);
 
+        //Make sure no motor speeds are over 1
         frontLeftMotor.setPower(frontLeftSpeed/max3);
         backLeftMotor.setPower(backLeftSpeed/max3);
         frontRightMotor.setPower(frontRightSpeed/max3);
         backRightMotor.setPower(backRightSpeed/max3);
+    }
+    public void drive(Gamepad gamepad1){
+        setMotorSpeeds(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
     }
 }
