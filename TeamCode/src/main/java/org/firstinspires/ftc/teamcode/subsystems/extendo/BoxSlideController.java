ce7ff413bcfc19d9e2208c3790db6b6189b47255
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2025-05-13 18:19:01 +1000

    Update BoxSlideController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/extendo/BoxSlideController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/extendo/BoxSlideController.java
@@ -5,8 +5,25 @@ import com.arcrobotics.ftclib.controller.PIDFController;
 
 public class BoxSlideController {
 
-    public void setExtendoPosition(PIDFController PIDF){
-        PIDF.setSetPoint(10);
-        PIDF.atSetPoint();
+    public void setExtendoPosition(PIDFController pidf){
+        // set our gains to some value
+        pidf.setP(0.37);
+        pidf.setI(0.05);
+        pidf.setD(1.02);
+
+        // get our gain constants
+        kP = pidf.getP();
+        kI = pidf.getI();
+        kD = pidf.getD();
+
+        // set all gains
+        pidf.setPIDF(kP, KI, kD, 0.7);
+
+        // get all gain coefficients
+        double[] coeffs = pidf.getCoefficients();
+        kP = coeffs[0];
+        kI = coeffs[1];
+        kD = coeffs[2];
+        kF = coeffs[3];
     }
 }
