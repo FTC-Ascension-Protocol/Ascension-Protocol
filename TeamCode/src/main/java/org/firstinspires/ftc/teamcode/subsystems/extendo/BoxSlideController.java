6dabdb7d532070efb816a8f2aa13eba9e5797c94
Sebmm116 <160821324+Sebmm116@users.noreply.github.com>
2025-05-13 18:15:45 +1000

    Update BoxSlideController.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/extendo/BoxSlideController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/extendo/BoxSlideController.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode.subsystems.extendo;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.controller.PIDFController;
+
 public class BoxSlideController {
 
+    public void setExtendoPosition(PIDFController PIDF){
+        PIDF.setSetPoint(10);
+        PIDF.atSetPoint();
+    }
 }
